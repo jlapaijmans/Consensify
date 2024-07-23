a6a70c9927abd1f84952847bd02851f17cb3c1b4
Andrea Manica <am315@cam.ac.uk>
2024-07-23 15:43:15 +0100

    Improve tests

--- test/test_consensify.R
+++ test/test_consensify.R
@@ -1,19 +1,23 @@
 # run tests with
 # Rscript ./test_consensify.R
 
-if(!require("testthat")) install.packages("testthat")
-if(!require("ape")) install.packages("ape")
+if(!require("testthat", quietly = TRUE)) install.packages("testthat")
+if(!require("ape", quietly = TRUE)) install.packages("ape")
 
 
 library(testthat)
 # remove the old test fasta file if it still exists
 if (file.exists("./test.fasta")){
   file.remove("./test.fasta")
+}
+if (file.exists("./test_gz.fasta")){
   file.remove("./test_gz.fasta")
 }
+
 # test with a simple set with full scaffolds
 test_that("consensify files with all scaffolds present",{
-  system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths.txt -o test.fasta -seed 123")
+  res <- system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths.txt -o test.fasta -seed 123",
+          stdout=TRUE)
   test <- ape::read.FASTA("./test.fasta")
   test <- as.character(test) ## cast it to character for easier testing
   expect_true(length(test)==2)
@@ -28,7 +32,8 @@ test_that("consensify files with all scaffolds present",{
   expect_true(test$scaffold1[59]=="n")
   expect_true(test$scaffold2[50]=="n")
   # now repeat with a gz file
-  system2("../consensify_c"," -c eg_missingness.counts.gz -p eg_missingness.pos.gz -s scaffold_lengths.txt -o test_gz.fasta -seed 123")
+  res <- system2("../consensify_c"," -c eg_missingness.counts.gz -p eg_missingness.pos.gz -s scaffold_lengths.txt -o test_gz.fasta -seed 123",
+                 stdout = TRUE)
   tools::md5sum("test.fasta") == tools::md5sum("test_gz.fasta")
   file.remove("./test.fasta")
   file.remove("./test_gz.fasta")
@@ -36,7 +41,8 @@ test_that("consensify files with all scaffolds present",{
 
 # test with a simple set with full scaffolds
 test_that("consensify files with min 2 and random reads 3",{
-  system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths.txt -o test.fasta -min 2 -max 99 -n_matches 2 -n_random_reads 3 -seed 123")
+  res <- system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths.txt -o test.fasta -min 2 -max 99 -n_matches 2 -n_random_reads 3 -seed 123",
+                 stdout=TRUE)
   test <- ape::read.FASTA("./test.fasta")
   test <- as.character(test) ## cast it to character for easier testing
   expect_true(length(test)==2)
@@ -50,7 +56,8 @@ test_that("consensify files with min 2 and random reads 3",{
   expect_true(test$scaffold1[58]=="n") # as depth is too low
   expect_true(test$scaffold1[59]=="n")
   expect_true(test$scaffold2[50]=="n")
-  system2("../consensify_c"," -c eg_missingness.counts.gz -p eg_missingness.pos.gz -s scaffold_lengths.txt -o test_gz.fasta -min 2 -max 99 -n_matches 2 -n_random_reads 3 -seed 123")
+  res <- system2("../consensify_c"," -c eg_missingness.counts.gz -p eg_missingness.pos.gz -s scaffold_lengths.txt -o test_gz.fasta -min 2 -max 99 -n_matches 2 -n_random_reads 3 -seed 123",
+          stdout = TRUE)
   tools::md5sum("test.fasta") == tools::md5sum("test_gz.fasta")
   file.remove("./test.fasta")
   file.remove("./test_gz.fasta")
@@ -58,7 +65,8 @@ test_that("consensify files with min 2 and random reads 3",{
 
 # test for missing scaffolds at beginning middle and end, without filling
 test_that("consensify files with missing scaffolds",{
-  system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths_missing_scaffold.txt -o test.fasta")
+  res <- system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths_missing_scaffold.txt -o test.fasta",
+          stdout = TRUE)
   test <- ape::read.FASTA("./test.fasta")
   test <- as.character(test) ## cast it to character for easier testing
   expect_true(length(test)==5)
@@ -71,7 +79,8 @@ test_that("consensify files with missing scaffolds",{
 
 # test for missing scaffolds at beginning middle and end, without filling
 test_that("consensify files with missing scaffolds, leaving them empty",{
-  system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths_missing_scaffold.txt -o test.fasta -no_empty_scaffold")
+  res <- system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths_missing_scaffold.txt -o test.fasta -no_empty_scaffold",
+          stdout = TRUE)
 test <- ape::read.FASTA("./test.fasta")
 test <- as.character(test) ## cast it to character for easier testing
 expect_true(length(test)==2)
@@ -84,5 +93,5 @@ file.remove("./test.fasta")
 #test we raise an error
 test_that("catch incorrect option",{
   err_code <- system2("../consensify_c"," -c eg_missingness.counts -p eg_missingness.pos -s scaffold_lengths_missing_scaffold.txt -o test.fasta -wrong_option")
-expect_true(err_code==1)
+  expect_true(err_code==1)
 })
